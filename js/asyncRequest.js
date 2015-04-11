0670b6fe99d8b6d6d550619ed6b793539fbd53c9
Michael J. Williams <me@mjwilliams.me>
2015-04-11 14:13:52 -0700

    Further abstraction
    Add ability to run asyncGet with json bool and your own callback
    function

--- js/asyncRequest.js
+++ js/asyncRequest.js
@@ -1,7 +1,15 @@
-// run request function
-asyncGet('timeframe.json');
+// example
+// asyncGet('timeframe.json', true, useAsyncData);
+// url of resource to request, json bool, function to call with asyncData
+// json bool should be true if resource is json, else false and you'll get text
+// callback function gets passed a json object if json===true, else text
 
-function asyncGet(url) {
+// callback example
+// function useAsyncData(data) {
+//	console.log(data);
+// }
+
+function asyncGet(url, json, fname) {
 	// var to contain async data
 	var asyncData;
 
@@ -25,17 +33,16 @@ function asyncGet(url) {
 		// http status 200 means server OK'd response (200: "OK")
 		// note that status should be 0 when retrieving from filesystem (without server)
 		if (xhr.readyState === 4 && xhr.status === 200) {
-			// if json
-			asyncData = JSON.parse(xhr.responseText);
-			// if text
-			// asyncData = xhr.responseText;
+			if (json === true) { // if json
+				asyncData = JSON.parse(xhr.responseText);
+			} else if (json === false) { // if text
+				asyncData = xhr.responseText;
+			} else console.log('asyncGet(json) argument was not a bool');
+		} else {
+			console.log('No response received yet, retrying...');
+			return;
 		}
 		// call a function that uses the data
-		useData(asyncData);
+		fname(asyncData);
 	}
-}
-
-// function that uses the data
-function useData(data) {
-	console.log(data);
 }
