fb9f596a4046d2c2221137dcc6d6a62925992d02
Michael J. Williams <me@mjwilliams.me>
2015-04-19 18:17:57 -0700

    Add error callback

--- js/asyncRequest.js
+++ js/asyncRequest.js
@@ -1,20 +1,29 @@
 // example
-// asyncGet('timeframe.json', true, useAsyncData);
+// asyncGet('timeframe.json', true, useAsyncData, errorCallback);
 // url of resource to request, json bool, function to call with asyncData
 // json bool should be true if resource is json, else false and you'll get text
 // callback function gets passed a json object if json===true, else text
+// error callback gets passed the url that failed to load
 
 // callback example
 // function useAsyncData(data) {
-//	console.log(data);
+//	   console.log(data);
 // }
 
-function asyncGet(url, json, fname) {
+// error callback example
+// function requestFailure(url) {
+//     alert('Failed to load ' + url);
+// }
+
+function asyncGet(url, json, cb, errcb) {
 	// var to contain async data
 	var asyncData;
 
 	// xhr object accessible in entire function
 	var xhr;
+
+	// number of request attempts; call errcb after 20 unsuccessful attempts
+	var attempts = 0;
 
 	// make async request
 	makeRequest(url);
@@ -31,18 +40,24 @@ function asyncGet(url, json, fname) {
 	function prepData() {
 		// readyState 4 means xhr request successful and response received from server
 		// http status 200 means server OK'd response (200: "OK")
-		// note that status should be 0 when retrieving from filesystem (without server)
+		// note that status should be 0 when retrieving from filesystem (without HTTP server)
 		if (xhr.readyState === 4 && xhr.status === 200) {
 			if (json === true) { // if json
 				asyncData = JSON.parse(xhr.responseText);
 			} else if (json === false) { // if text
 				asyncData = xhr.responseText;
 			} else console.log('asyncGet(json) argument was not a bool');
+			// call a function that uses the data
+			cb(asyncData);
 		} else {
-			console.log('asyncGet(' + url + '): No response received yet, retrying...');
-			return;
+			attempts++;
+			console.log('asyncGet(' + url + '): No response received yet (attempt ' + attempts + '), retrying...');
+			// after 4 failed requests
+			if (attempts >= 4) {
+				console.log('asyncGet(' + url + '): failed to load resource after ' + attempts + ' attempts. Will now call errcb if given.');
+				// call error cb with url
+				errcb(url);
+			} else return; // return, browser will try again
 		}
-		// call a function that uses the data
-		fname(asyncData);
 	}
 }
