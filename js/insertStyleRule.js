17ecc5f4f488cf61ba00b52c66d19a825645a73c
Michael J. Williams <me@mjwilliams.me>
2015-04-19 18:26:54 -0700

    Patch for multiple style tags

--- js/insertStyleRule.js
+++ js/insertStyleRule.js
@@ -1,12 +1,18 @@
 // inserts style rule at end of first style tag
 // This is necessary to modify a pseudo element css rule because pseudo elements do not exist in the DOM.
+
+// example
+// insertStyleRule('body {background-color:#444444}');
+
 function insertStyleRule(newRule) {
 	// htmlcollection (array) of all style elements
 	var styleTags = document.getElementsByTagName('style');
 
-	// pick first in array
+	// pick last style tag in array so that the new rule supersedes other rules
+	var indexOfLast = styleTags.length - 1;
+
 	// var style becomes an object with a bunch of css/dom info
-	var style = styleTags[0].sheet;
+	var style = styleTags[indexOfLast].sheet;
 
 	// determines number of rules in stylesheet (cssRules is an array containing all of the style rules)
 	// array is zero-indexed, so .length will return a value 1 greater than the index of last rule
@@ -15,5 +21,3 @@ function insertStyleRule(newRule) {
 	// new rule is inserted at index which is 1 greater than index of last rule
 	style.insertRule(newRule, numRules);
 }
-
-insertStyleRule('body {background-color:#444444}');
