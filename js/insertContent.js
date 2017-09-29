e80e395bab6548992f2387405407539e6e095dd3
michaeljwilliams <michael@williams.blue>
2017-09-28 22:48:38 -0700

    fix for Safari 9
    - Safari 9 doesn't support default parameters for functions

--- js/insertContent.js
+++ js/insertContent.js
@@ -3,10 +3,10 @@
 // [addOrReplace] determines whether to simply add the content or replace the destination content.
 // [addOrReplace] should be a string of "add" or "replace". It defaults to add
 // example: insertContent("main", "<h1>hello!</h1>"); // inserts an <h1> into the <main> element
-function insertContent(where, content, addOrReplace = "add") {
+function insertContent(where, content, addOrReplace) { // Safari 9 doesn't support default parameters
     if(addOrReplace === "replace") {
         document.querySelector(where).innerHTML = content;
-    } else if(addOrReplace === "add" ) {
+    } else if(addOrReplace !== "replace" ) {
         document.querySelector(where).innerHTML += content;
     }
 }
